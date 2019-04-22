6d942e474986e412940fcd7a80013c26dfbaa74d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-22 16:59:31 -0400

    Added a list of fake users.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/User';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    this.users = [
+      {
+      firstName: 'Ellie',
+      lastName: 'Barr',
+      age: 43,
+      address: {
+          street: '12 East Side St',
+          city: 'Los Angeles',
+          state: 'CA'
+      }
+    },
+    {
+      firstName: 'Jessica',
+      lastName: 'Smith',
+      age: 25,
+      address: {
+          street: '22 Flower Lane',
+          city: 'Charleston',
+          state: 'SC'
+      }
+    },
+    {
+      firstName: 'Eric',
+      lastName: 'Doe',
+      age: 18,
+      address: {
+          street: '5 First St',
+          city: 'Pheonix',
+          state: 'AZ'
+      }
+    },
+    ];
   }
 
 }
