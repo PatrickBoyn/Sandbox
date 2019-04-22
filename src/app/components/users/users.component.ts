b7722014aea92c561cc81675673b384a389b50d8
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-04-22 18:38:47 -0400

    Added a timeout for a fake loading page.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,54 +7,51 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-
+  showExtended = true;
+  loaded = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
+    setTimeout(() => {
+      this.users = [
+        {
+        firstName: 'Ellie',
+        lastName: 'Barr',
+        age: 43,
+        address: {
+            street: '12 East Side St',
+            city: 'Los Angeles',
+            state: 'CA'
+        }
+      },
       {
-      firstName: 'Ellie',
-      lastName: 'Barr',
-      age: 43,
-      address: {
-          street: '12 East Side St',
-          city: 'Los Angeles',
-          state: 'CA'
+        firstName: 'Jessica',
+        lastName: 'Smith',
+        age: 25,
+        address: {
+            street: '22 Flower Lane',
+            city: 'Charleston',
+            state: 'SC'
+        }
+      },
+      {
+        firstName: 'Eric',
+        lastName: 'Doe',
+        age: 18,
+        address: {
+            street: '5 First St',
+            city: 'Pheonix',
+            state: 'AZ'
+        }
       }
-    },
-    {
-      firstName: 'Jessica',
-      lastName: 'Smith',
-      age: 25,
-      address: {
-          street: '22 Flower Lane',
-          city: 'Charleston',
-          state: 'SC'
-      }
-    },
-    {
-      firstName: 'Eric',
-      lastName: 'Doe',
-      age: 18,
-      address: {
-          street: '5 First St',
-          city: 'Pheonix',
-          state: 'AZ'
-      }
-    }
-    ];
-
-    this.addUser({
+      ];
+          this.addUser({
       firstName: 'Anne',
       lastName: 'Forrest',
-      age: 21,
-      address: {
-          street: '5 Second St',
-          city: 'Seattle',
-          state: 'Washington'
-      }
     });
+    this.loaded = true;
+    }, 2000);
   }
 
   addUser(user: User) {
